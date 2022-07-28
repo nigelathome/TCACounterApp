0927f30c4a4f4198ba7b72a14f3b008080ed0993
Hui Li <hui.li@compass.com>
2022-07-28 17:25:59 +0800

    EditView

--- TCACounterApp/EditCountent/EditCountentView.swift
+++ TCACounterApp/EditCountent/EditCountentView.swift
@@ -11,20 +11,20 @@ import ComposableArchitecture
 
 struct EditCountentView: View {
     let store: Store<CounterDetailState, CounterDetailAction>
-//    @State var count = ""
+    @State var count = ""
     var body: some View {
-        Text("111")
-//        WithViewStore(store) {ViewStore in
-//            TextField("", text: $count, onEditingChanged: { changed in
-//                print("onEditing: \(changed)")
-//            }) {
-//                print("userName: \(self.$count)")
-//            }
-//            .padding(10)
-//            .frame(height: 50)
-//            .textFieldStyle(RoundedBorderTextFieldStyle())
-//            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-//        }
+//        Text("111")
+        WithViewStore(store) {ViewStore in
+            TextField("", text: $count, onEditingChanged: { changed in
+                print("onEditing: \(changed)")
+            }) {
+                print("count: \(self.$count)")
+            }
+            .padding(10)
+            .frame(height: 50)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        }
     }
 
 }
